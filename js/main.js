0aca43913c6cbef1e27a1bbcc30957bdb0b44eed
B.Alexia Young <brlxyng@gmail.com>
2018-04-15 11:57:45 -0600

    styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // main.js//
+$(document).ready(function(){$('video, audio').mediaelementplayer();});
+//add control buttons to video
 $("video").mediaelementplayer({
     features:['playpause','progress', 'volume', 'fullscreen']
 });
@@ -6,50 +8,42 @@ $("video").mediaelementplayer({
 
 //variables
 const subtext= document.getElementsByClassName("hiLightText");
-console.log(subtext)
 let vid = document.getElementById("vidItem");
 const wrapper= document.getElementsByClassName('wrapper');
-//const tracksub= subtext.length + 1;
-
+let width= 800 +"px";
+let height= 500 +"px";
 
 // add timeupdate listener and function for highlighting text while video plays.
 vid.addEventListener('timeupdate', function(){timefunction();});
 
 function timefunction(){
     let playTime=(vid.currentTime);
-    //console.log('trace');
-    //console.log(playTime);
 for(let i=0; i < subtext.length; i++){
-    //console.log('lizzy');
     if (playTime >= subtext[i].getAttribute("data-start") && playTime <= subtext[i].getAttribute("data-end")){
-    //if (playTime > subtext[i].dataset.start && playtime < subtext[i].dataset.end){
-//console.log(subtext[i].getAttribute("data-start"));
         subtext[i].style.backgroundColor="yellow";
-        //console.log('playTime');
-//console.log('subtext[i].getAttribute("data-start")')
     } else {
         subtext[i].style.backgroundColor="white";
     }
 }
 };
 
-// add click listener to spans for matching video playback.
-//function subToVideo() {
+// add click listener to document and filter for "hiLightText" class only for matching video playback.
 
- //   subtext.play= subtext.getAttribute("data-start");
-//};
-// wrapper.addEventListener('click', (event));
-//     //subtext.play= subtext.getAttribute("data-start");
-// if (event.target.class == 'hiLightText') {
-//             subtext.play = subtext.getAttribute("data-start");
-
-// };
 document.addEventListener('click', (event) => {
     let playThis= event.target;
-console.log(playThis);
     if (playThis.className == 'hiLightText') {
         let getspan=event.target;
         vid.currentTime = getspan.getAttribute('data-start') ;
         vid.play();
     };
 });
+
+const mediaQry = (x) => {
+    if (x.matches) { // If media query matches
+        vid.setAttribute("width", width);
+        vid.setAttribute("height", height);
+    }
+}
+var x = window.matchMedia("(min-width: 768px)")
+mediaQry(x) // Call listener function at run time
+x.addListener(mediary) // Attach listener function on state changes
